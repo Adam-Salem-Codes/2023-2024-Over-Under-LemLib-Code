ab4c442e130c6d6dced2592f79ffbe9565d1c2e4
Adam Salem <84548368+Adam-Salem-Codes@users.noreply.github.com>
2024-02-03 18:35:50 -0500

    Create selection.h

---
+++ include/selection.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include "main.h"
+
+// selector configuration
+#define HUE 130
+#define DEFAULT 1
+#define AUTONS "Front", "Back", "Do Nothing"
+
+namespace selector
+{
+
+    extern int auton;
+    extern const char *b[];
+    void init(int hue = HUE, int default_auton = DEFAULT, const char **autons = b);
+}
